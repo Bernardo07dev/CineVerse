a76495ce59efaa0b780a66d1cd3da567c1d2ccbb
Bernardo07dev <bernardomoreira.dev@gmail.com>
2025-10-13 09:42:15 -0300

    Install React

---
+++ src/CineVerse.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const CineVerse = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Cineverse</h1>
+    </div>
+  )
+}
+
+export default CineVerse
